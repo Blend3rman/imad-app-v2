4a9f287584e27b34b95ccaf6759eee7638719c4a
Yogeshwar Bala <yogeshwarb404@gmail.com>
2017-02-01 18:49:38 +0530

    [imad-console] Updates server.js

--- server.js
+++ server.js
@@ -9,6 +9,18 @@ app.get('/', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'index.html'));
 });
 
+app.get('/one', function (req, res) {
+  res.send('Article One Requested. Served here.');    
+});
+
+app.get('/two', function (req, res) {
+  res.send('Article Two Requested. Served here.');    
+});
+
+app.get('/three', function (req, res) {
+  res.send('Article Three Requested. Served here.');    
+});
+
 app.get('/ui/style.css', function (req, res) {
   res.sendFile(path.join(__dirname, 'ui', 'style.css'));
 });
